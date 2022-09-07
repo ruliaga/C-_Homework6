3657d0dfd65dac25ae782ff78fabdde58a268010
[Ruliaga] <ruliaga@mail.ru>
2022-09-08 03:04:53 +0500

    Task43 done

---
+++ Task43/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите k1");
+double k1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b1");
+double b1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2=Convert.ToDouble(Console.ReadLine());
+
+double x=(b2-b1)/(k1-k2);
+double y=k1*x+b1;
+
+Console.WriteLine($"k1={k1}, k2={k2}, b1={b1}, b2={b2} -> ({x};{y})");
